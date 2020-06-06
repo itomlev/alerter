96bf929cfdbf71b6c1b6b5812b7a4603a3bf5cd1
tomlev <tom@powerinbox.com>
2020-06-06 14:43:07 +0300

    api structure

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
 const express = require('express');
+const redis = require('redis');
+const redisClient = redis.createClient();
 const router = express.Router();
 
-router.post('/echoAtTime', function (req, res, next) {
+router.post('/echoAtTime', (req, res, next) => {
 	console.log(`POST /echoAtTime`);
-	res.sendStatus(200);
+
+	// validate input
+	if (!validate(req.body)) {
+		return res.status(400).json({status: 'fail', message: 'invalid input'});
+	}
+	// save in Redis
+
+	res.status(200).json({status: 'success', message: 'message has been registered successfully'});
 });
+
+validate = (req) => {
+	if (!req.time || !req.message) {
+		return false;
+	}
+	return true;
+};
 
 module.exports = router;
